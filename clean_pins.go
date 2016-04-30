60d0075df46274936fe60d7f59e1e778305aa83c
mose <mose@mose.com>
2016-04-30 19:57:57 +0800

    prepare a clean_pins thing

---
+++ clean_pins.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "regexp"
+  "encoding/json"
+  "github.com/nlopes/slack"
+)
+
+type Config struct {
+  Key      string
+  Channel  string
+  Launcher string
+}
+
+func main() {
+
+  file, _ := os.Open("config.json")
+  decoder := json.NewDecoder(file)
+  config := Config{}
+  err := decoder.Decode(&config)
+  if err != nil {
+    fmt.Println("error:", err)
+    return
+  }
+
+  api := slack.New(config.Key)
+  pins, _, err := api.ListPins(config.Channel)
+  if err != nil {
+    fmt.Printf("%s\n", err)
+    return
+  }
+  for _, pin := range pins {
+    
+    fmt.Println(config.Launcher, url)
+  }
+}
