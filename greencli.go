330527801f8a6f6a001244eef2f539f6e4b9bd68
mose <mose@mose.com>
2016-05-05 11:37:31 +0800

    base cli setup

--- greencli.go
+++ greencli.go
@@ -8,5 +8,14 @@ import (
 )
 
 func main() {
-  cli.NewApp().Run(os.Args)
+  app := cli.NewApp()
+  app.Name = "greencli"
+  app.Version = "0.0.1"
+  app.Usage = "A multipurpose CLI for Green Ruby publication system."
+  app.Action = func(c *cli.Context) error {
+    fmt.Println("boom! I say!")
+    return nil
+  }
+
+  app.Run(os.Args)
 }
